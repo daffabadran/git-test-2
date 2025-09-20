f0821d9815a5560440756a4d754d142b4482ae4b
Daffa Badran <daffabadrann@gmail.com>
2025-09-20 14:58:38 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur ex reprehenderit iure aut unde est amet ipsum in! Cumque, quibusdam.</p>
+
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
