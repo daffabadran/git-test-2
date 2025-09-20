e2edb5924fd4b16e2701894c1f3dbbfe5d8c47f8
Daffa Badran <daffabadrann@gmail.com>
2025-09-20 14:54:34 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur ex reprehenderit iure aut unde est amet ipsum in! Cumque, quibusdam.</p>
 </body>
 </html>
